50fc34c656ab78748588f5c5ba78013e166162b1
JuanGQCadavid <jquirozcadavid@gmail.com>
2021-11-10 21:23:16 -0500

    It is working! Now it shows the route to a point!

--- front/now/lib/delivery_screen.dart
+++ front/now/lib/delivery_screen.dart
@@ -2,10 +2,12 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:now/directions_provider.dart';
+import 'package:provider/provider.dart';
 
 class DeliveryScreen extends StatefulWidget {
-  final LatLng fromPoint = const LatLng(-38.956176, -67.920666);
-  final LatLng toPoint = const LatLng(-38.953724, -67.923921);
+  final LatLng fromPoint = const LatLng(6.244725272333634, -75.5889493512081);
+  final LatLng toPoint = const LatLng(6.2464643393698855, -75.59065451559682);
 
   const DeliveryScreen({Key? key}) : super(key: key);
 
@@ -31,13 +33,36 @@ class _DeliveryScreenState extends State<DeliveryScreen> {
   }
 
   Widget _map() {
+    return Consumer<DirectionsProvider>(
+      builder:
+          (BuildContext context, DirectionsProvider provider, Widget? child) {
+        return Stack(
+          children: [
+            _googleMap(provider),
+            Center(
+                child: Container(
+              child: Text(
+                provider.distanceTime,
+                style: const TextStyle(fontSize: 25),
+              ),
+              color: Colors.amber,
+            )),
+          ],
+        );
+      },
+    );
+  }
+
+  GoogleMap _googleMap(DirectionsProvider provider) {
     return GoogleMap(
       initialCameraPosition: CameraPosition(target: widget.fromPoint, zoom: 16),
       markers: _createMarkers(),
       onMapCreated: _onMapCreated,
       mapToolbarEnabled: false,
-      myLocationButtonEnabled: false,
+      myLocationButtonEnabled: true,
       zoomControlsEnabled: false,
+      polylines: provider.currentRoute,
+      myLocationEnabled: true,
     );
   }
 
@@ -69,12 +94,20 @@ class _DeliveryScreenState extends State<DeliveryScreen> {
 
   void _centerView() async {
     await _controller.getVisibleRegion();
+    var api = Provider.of<DirectionsProvider>(context, listen: false);
+    await api.findDirections(widget.fromPoint, widget.toPoint);
 
     var left = min(widget.fromPoint.latitude, widget.toPoint.latitude);
     var right = max(widget.fromPoint.latitude, widget.toPoint.latitude);
-
     var top = max(widget.fromPoint.longitude, widget.toPoint.longitude);
     var down = min(widget.fromPoint.longitude, widget.toPoint.longitude);
+
+    api.currentRoute.first.points.forEach((point) {
+      left = min(left, point.latitude);
+      right = max(right, point.latitude);
+      top = max(top, point.longitude);
+      down = min(down, point.longitude);
+    });
 
     var bounds = LatLngBounds(
         southwest: LatLng(left, down), northeast: LatLng(right, top));
--- front/now/lib/main.dart
+++ front/now/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:now/delivery_screen.dart';
+import 'package:now/directions_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,21 +13,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.blue,
-        ),
-        home: const DeliveryScreen());
+    return ChangeNotifierProvider(
+      create: (_) => DirectionsProvider(),
+      child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const DeliveryScreen()),
+    );
   }
 }
 
