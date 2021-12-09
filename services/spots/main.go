d37d3343a603870c50ac3628fea751087027969b
JuanGQCadavid <jquirozcadavid@gmail.com>
2021-12-08 21:15:03 -0500

    Testing with strucs

--- services/spots/main.go
+++ services/spots/main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import "fmt"
 
+type Salary struct {
+	Basic, HRA, TA float64
+}
+
+type Employee struct {
+	FirstName, LastName, Email string
+	Age                        int
+	MonthlySalary              []Salary
+}
+
+func main() {
+	e := Employee{
+		FirstName: "Mark",
+		LastName:  "Jones",
+		Email:     "mark@gmail.com",
+		Age:       25,
+		MonthlySalary: []Salary{
+			{
+				Basic: 15000.00,
+				HRA:   5000.00,
+				TA:    2000.00,
+			},
+			{
+				Basic: 16000.00,
+				HRA:   5000.00,
+				TA:    2100.00,
+			},
+			{
+				Basic: 17000.00,
+				HRA:   5000.00,
+				TA:    2200.00,
+			},
+		},
+	}
+	fmt.Println(e.FirstName, e.LastName)
+	fmt.Println(e.Age)
+	fmt.Println(e.Email)
+	fmt.Println(e.MonthlySalary[0])
+	fmt.Println(e.MonthlySalary[1])
+	fmt.Println(e.MonthlySalary[2])
 }
