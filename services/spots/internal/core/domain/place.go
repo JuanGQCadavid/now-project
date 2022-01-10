f468ac44f1ff724cf07e2a761b7f491f0f237f59
JuanGQCadavid <jquirozcadavid@gmail.com>
2022-01-10 13:39:59 -0500

    Working towards neo4j integration

--- services/spots/internal/core/domain/place.go
+++ services/spots/internal/core/domain/place.go
@@ -1,6 +1,7 @@
 package domain
 
 type Place struct {
+	Name          string  `json:"name"`
 	Lat           float32 `json:"lat"` // those nas are tags!
 	Lon           float32 `json:"lon"`
 	MapProviderId int32   `json:"mapProviderId"`
--- services/spots/internal/repositories/neo4jRepository/neo4jSpotRepo.go
+++ services/spots/internal/repositories/neo4jRepository/neo4jSpotRepo.go
@@ -1,8 +1,11 @@
 package neo4jRepository
 
 import (
+	"fmt"
+
 	"github.com/JuanGQCadavid/now-project/services/spots/internal/core/domain"
 	"github.com/neo4j/neo4j-go-driver/neo4j"
+	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
 )
 
 type Neo4jSpotRepo struct {
@@ -16,16 +19,43 @@ func NewNeo4jSpotRepo() *Neo4jSpotRepo {
 	}
 }
 
-func (r Neo4jSpotRepo) Get(id string) (domain.Spot, error) {
+func (r Neo4jSpotRepo) Get(id string) (*domain.Spot, error) {
 	session := r.neo4jRepoDriver.driver.NewSession(neo4j.SessionConfig{})
 
-	return domain.Spot{}, nil
+	records, err := session.ReadTransaction(func(tr neo4j.TransactionConfig) (interface{}, error) {
+		return r.getSpot(tr, id)
+	})
+
+	if err != nil {
+		return &domain.Spot{}, err
+	}
+
+	fmt.Println(records)
+	spot := records.(*domain.Spot)
+	fmt.Println(spot)
+	return spot, nil
 }
+
+func (r Neo4jSpotRepo) getSpot(tr *neo4j.TransactionConfig, spotId string) (*domain.Spot, error) {
+
+	return &domain.Spot{}, nil
+}
+
 func (r Neo4jSpotRepo) CreateOnline(spot domain.Spot) error {
+	session := r.neo4jRepoDriver.driver.NewSession(neo4j.SessionConfig{})
+	records, err := session.WriteTransaction(func(tr neo4j.TransactionConfig) (interface {}, error) {
+		return 
+	})
 	return nil
 }
-func (r Neo4jSpotRepo) GetSpotByUserId(personId string) (domain.Spot, error) {
-	return domain.Spot{}, nil
+
+func (r Neo4jSpotRepo) createEvent( tr *neo4j.TransactionConfig, spot domain.Spot)  (*domain.Spot, error) {
+
+}
+
+
+func (r Neo4jSpotRepo) GetSpotByUserId(personId string) (*domain.Spot, error) {
+	return &domain.Spot{}, nil
 }
 func (r Neo4jSpotRepo) EndSpot(spotId string) error {
 	return nil
