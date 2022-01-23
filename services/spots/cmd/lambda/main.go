7ced83e6de531159fad4203ed23fcd08ea77e571
JuanGQCadavid <jquirozcadavid@gmail.com>
2022-01-23 14:53:38 -0500

    Lambda handler working!

--- services/spots/cmd/lambda/main.go
+++ services/spots/cmd/lambda/main.go
@@ -1,26 +1,45 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
 
+	"github.com/JuanGQCadavid/now-project/services/spots/internal/core/services/spotsrv"
+	"github.com/JuanGQCadavid/now-project/services/spots/internal/handlers/httphdl"
+	"github.com/JuanGQCadavid/now-project/services/spots/internal/repositories/menRepository"
+	"github.com/JuanGQCadavid/now-project/services/spots/internal/repositories/neo4jRepository"
+	"github.com/JuanGQCadavid/now-project/services/spots/pkg/uuidgen"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
+	"github.com/gin-gonic/gin"
 )
 
-type MyEvent struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
+var ginLambda *ginadapter.GinLambda
+
+func init() {
+	// stdout and stderr are sent to AWS CloudWatch Logs
+	log.Printf("Gin cold start")
+
+	repoSpot := neo4jRepository.NewNeo4jSpotRepo() //menRepository.New()
+	repoLocation := menRepository.NewLocationRepository()
+	uuid := uuidgen.New()
+
+	service := spotsrv.New(repoSpot, repoLocation, uuid)
+	httpHandler := httphdl.NewHTTPHandler(service)
+
+	router := gin.Default()
+	router.GET("/spot/:id", httpHandler.GetEvent)
+	router.POST("/spot/online", httpHandler.GoOnline)
+
+	ginLambda = ginadapter.New(router)
 }
 
-type MyResponse struct {
-	Message string `json:"Answer:"`
-}
-
-func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
-	println("HIIII")
-	return MyResponse{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// If no name is provided in the HTTP request body, throw an error
+	return ginLambda.ProxyWithContext(ctx, req)
 }
 
 func main() {
-	println("Dude")
-	lambda.Start(HandleLambdaEvent)
+	lambda.Start(Handler)
 }
