e3c939fb80345d05b89f84deab3a2494c3e6d007
JuanGQCadavid <jquirozcadavid@gmail.com>
2022-01-23 13:41:13 -0500

    Apigateway + spot method

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,4 +1,5 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { aws_apigateway as apigateway, aws_lambda as lambda } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +7,33 @@ export class InfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    function addMethodToApiGateway(lambdaToAdd: lambda.Function, apigatewayToHandle: apigateway.RestApi, methodName: string ): apigateway.Resource{
+      const spotMethod = apigatewayToHandle.root.addResource(methodName);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+      const lambdaIntegration = new apigateway.LambdaIntegration(lambdaToAdd, {
+        proxy: true
+      });
+
+      spotMethod.addMethod('GET', lambdaIntegration);
+      spotMethod.addMethod('POST', lambdaIntegration);
+      spotMethod.addMethod('PUT', lambdaIntegration);
+      spotMethod.addMethod('DELETE', lambdaIntegration);
+
+      return spotMethod;
+    }
+
+    const mainApiGateway = new apigateway.RestApi(this, 'mainApiGateway', {
+
+    })
+
+    mainApiGateway.root.addMethod("ANY");
+
+    const spotLambda = new lambda.Function(this, 'spotLambda', {
+      runtime : lambda.Runtime.GO_1_X,
+      handler: 'main',
+      code: lambda.Code.fromInline('')
+    })
+
+    addMethodToApiGateway(spotLambda, mainApiGateway, "spot")
   }
 }
