d4c1ceed1f0a8460c9062a78d67c13adfff90302
JuanGQCadavid <jquirozcadavid@gmail.com>
2022-01-24 23:53:05 -0500

    Adding maps widget

--- front/now/lib/spots_general_view.dart
+++ front/now/lib/spots_general_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'spots_granular_view.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class SpotGeneralView extends StatelessWidget {
   const SpotGeneralView({Key? key}) : super(key: key);
@@ -42,9 +43,37 @@ class _Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: _MapBody(),
       decoration: const BoxDecoration(
         color: Colors.blueGrey,
       ),
     );
+  }
+}
+
+class _MapBody extends StatefulWidget {
+  const _MapBody({Key? key}) : super(key: key);
+
+  @override
+  __MapBodyState createState() => __MapBodyState();
+}
+
+class __MapBodyState extends State<_MapBody> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(6.246408, -75.590666);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 20.0,
+        ));
   }
 }
