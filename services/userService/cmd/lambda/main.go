f6caf7df1c4d943b84b220dfb9fbab59debdde23
JuanGQCadavid <jquirozcadavid@gmail.com>
2023-12-02 23:28:45 -0500

    Testing pipeline

--- services/userService/cmd/lambda/main.go
+++ services/userService/cmd/lambda/main.go
@@ -60,7 +60,6 @@ func getenv(key, fallback string) string {
 }
 
 func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	// If no name is provided in the HTTP request body, throw an error
 	return ginLambda.ProxyWithContext(ctx, req)
 }
 
