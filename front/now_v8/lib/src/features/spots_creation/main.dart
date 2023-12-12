649e36cab1809b701b9228db3bc5b97fb21a5c5e
JuanGQCadavid <jquirozcadavid@gmail.com>
2023-12-11 22:43:12 -0500

    Init Spot creation

---
+++ front/now_v8/lib/src/features/spots_creation/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+
+class SpotsCreationFeature extends StatelessWidget {
+  const SpotsCreationFeature({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
