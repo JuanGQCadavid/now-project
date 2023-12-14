437b65695ea62241427627f559ca9024a9933a64
JuanGQCadavid <jquirozcadavid@gmail.com>
2023-12-13 20:24:44 -0500

    Statusbar prototype

--- front/now_v8/lib/src/features/spots_creation/main.dart
+++ front/now_v8/lib/src/features/spots_creation/main.dart
@@ -7,7 +7,89 @@ class SpotsCreationFeature extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Placeholder(),
+        child: Body(),
+      ),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            constraints: const BoxConstraints(
+              maxHeight: double.infinity,
+            ),
+            child: Center(
+              child: Text("Hi dude how are you? "),
+            ),
+          ),
+        ),
+        Container(
+          constraints: const BoxConstraints(
+            maxHeight: double.infinity,
+            minWidth: 50,
+            maxWidth: 50,
+          ),
+          decoration: BoxDecoration(color: Colors.amber.shade100),
+          child: Center(
+            child: StatusBar(),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class StatusBar extends StatelessWidget {
+  const StatusBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Status(),
+        Status(),
+        Status(),
+      ],
+    );
+  }
+}
+
+class Status extends StatelessWidget {
+  const Status({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 35,
+      width: 35,
+      decoration: const BoxDecoration(
+        color: Colors.green,
+        borderRadius: BorderRadius.all(
+          Radius.circular(50),
+        ),
+      ),
+      child: Center(
+        child: Container(
+          height: 30,
+          width: 30,
+          decoration: const BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.all(
+              Radius.circular(50),
+            ),
+          ),
+          child: const Center(
+            child: Text("1"),
+          ),
+        ),
       ),
     );
   }
