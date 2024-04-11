8869cd309503e4038cf014943ebfd99f8452af5f
JuanGQCadavid <jquirozcadavid@gmail.com>
2024-04-10 19:26:18 -0500

    It is working

--- services/authService/cmd/lambda/main.go
+++ services/authService/cmd/lambda/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type MyEvent struct {
+	Name string `json:"name"`
+}
+
+func Handler(ctx context.Context, event MyEvent) (string, error) {
+	fmt.Println(event)
+	return "Hi", nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
--- services/authService/go.mod
+++ services/authService/go.mod
@@ -1,3 +1,5 @@
 module github.com/JuanGQCadavid/now-project/services/authService
 
 go 1.20
+
+require github.com/aws/aws-lambda-go v1.46.0 // indirect
