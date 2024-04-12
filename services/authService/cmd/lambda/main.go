00d2d7092d693545fe067a8c394426d09275e9b3
JuanGQCadavid <jquirozcadavid@gmail.com>
2024-04-12 16:25:50 -0500

    Lambda api gateway integration done

--- services/authService/cmd/lambda/main.go
+++ services/authService/cmd/lambda/main.go
@@ -2,18 +2,56 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"strings"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type MyEvent struct {
-	Name string `json:"name"`
+func Handler(ctx context.Context, event events.APIGatewayV2CustomAuthorizerV2Request) (events.APIGatewayCustomAuthorizerResponse, error) {
+	fmt.Printf("The event: %+v\n", event)
+	token := event.Headers["x-auth"]
+
+	fmt.Println("token:", token)
+	fmt.Println("Headers:", event.Headers)
+
+	switch strings.ToLower(token) {
+	case "allow":
+		return generatePolicy("user", "Allow", event.RouteArn), nil
+	case "deny":
+		return generatePolicy("user", "Deny", event.RouteArn), nil
+	case "unauthorized":
+		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Unauthorized") // Return a 401 Unauthorized response
+	default:
+		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("err: Invalid token")
+	}
 }
 
-func Handler(ctx context.Context, event MyEvent) (string, error) {
-	fmt.Println(event)
-	return "Hi", nil
+func generatePolicy(principalId, effect, resource string) events.APIGatewayCustomAuthorizerResponse {
+	authResponse := events.APIGatewayCustomAuthorizerResponse{PrincipalID: principalId}
+
+	if effect != "" && resource != "" {
+		authResponse.PolicyDocument = events.APIGatewayCustomAuthorizerPolicy{
+			Version: "2012-10-17",
+			Statement: []events.IAMPolicyStatement{
+				{
+					Action:   []string{"execute-api:Invoke"},
+					Effect:   effect,
+					Resource: []string{resource},
+				},
+			},
+		}
+	}
+
+	// Optional output with custom properties of the String, Number or Boolean type.
+	authResponse.Context = map[string]interface{}{
+		"stringKey":  "stringval",
+		"numberKey":  123,
+		"booleanKey": true,
+	}
+	return authResponse
 }
 
 func main() {
