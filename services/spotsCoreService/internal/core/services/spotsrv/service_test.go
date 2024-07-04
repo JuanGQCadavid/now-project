fce97bedcb56fea66a56ed0711803e957b8d02c6
Juan Gonzalo Quıroz Cadavıd <jquirozcadavid@gmail.com>
2024-07-04 22:00:00 +0300

    Starting with unit testing

---
+++ services/spotsCoreService/internal/core/services/spotsrv/service_test.go
@@ -0,0 +1,23 @@
+package spotsrv
+
+import "testing"
+
+func TestService_Get(t *testing.T) {
+
+}
+
+func TestService_GetSpotsByDatesIds(t *testing.T) {
+
+}
+func TestService_CreateSpot(t *testing.T) {
+
+}
+func TestService_UpdateSpotEvent(t *testing.T) {
+
+}
+func TestService_UpdateSpotTopic(t *testing.T) {
+
+}
+func TestService_UpdateSpotPlace(t *testing.T) {
+
+}
