b6fcfbfb9e6c574d07824edc9fde10532919a4da
Juan Gonzalo Quıroz Cadavıd <jquirozcadavid@gmail.com>
2024-07-15 11:43:27 +0300

    :sparkles: Docker for both services done, template configured and dummy code on sqs handler

--- services/confirmationService/cmd/sqs/main.go
+++ services/confirmationService/cmd/sqs/main.go
@@ -2,57 +2,17 @@ package main
 
 import (
 	"context"
-
-	"github.com/JuanGQCadavid/now-project/services/confirmationService/internal/core/service"
-	"github.com/JuanGQCadavid/now-project/services/confirmationService/internal/handlers/httphdl"
-	"github.com/JuanGQCadavid/now-project/services/confirmationService/internal/notifiers/topics"
-	"github.com/JuanGQCadavid/now-project/services/confirmationService/internal/repositories/neo4j"
 	"github.com/JuanGQCadavid/now-project/services/pkgs/common/logs"
-	"github.com/JuanGQCadavid/now-project/services/pkgs/credentialsFinder/cmd/ssm"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
-	"github.com/gin-gonic/gin"
 )
-
-const (
-	TopicArnEnvName = "snsArn"
-)
-
-var ginLambda *ginadapter.GinLambda
-
-func init() {
-	logs.Info.Println("Gin cold start")
-
-	credsFinder := ssm.NewSSMCredentialsFinder()
-	neo4jDriver, err := credsFinder.FindNeo4jCredentialsFromDefaultEnv()
-
-	if err != nil {
-		logs.Error.Println("There were an error while attempting to create drivers")
-		logs.Error.Fatalln(err.Error())
-	}
-
-	repoSpot := neo4j.NewNeo4jRepoWithDriver(neo4jDriver)
-	notifier, err := topics.NewNotifierFromEnv(TopicArnEnvName)
-
-	if err != nil {
-		logs.Error.Fatalln("We have a problem seting up the server, notifer error", err.Error())
-	}
-
-	service := service.NewConfirmationService(repoSpot, notifier)
-	httpHandler := httphdl.NewHttpHandler(service)
-
-	router := gin.Default()
-	httpHandler.SetRouter(router)
-
-	ginLambda = ginadapter.New(router)
-}
-
-func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	// If no name is provided in the HTTP request body, throw an error
-	return ginLambda.ProxyWithContext(ctx, req)
-}
 
 func main() {
 	lambda.Start(Handler)
+}
+
+func Handler(ctx context.Context, body *events.SQSEvent) (string, error) {
+	logs.Info.Println("Greetings form the sqs handler my boy!")
+	logs.Info.Printf("%+v", body.Records)
+	return "Okey", nil
 }
