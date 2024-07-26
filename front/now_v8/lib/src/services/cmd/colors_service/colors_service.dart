faf182d418ac9d686b548f96fa05e4dfe73f6854
Juan Gonzalo Quıroz Cadavıd <jquirozcadavid@gmail.com>
2024-07-26 12:13:05 +0300

    :construction: Cleaning code for colors service

--- front/now_v8/lib/src/services/cmd/colors_service/colors_service.dart
+++ front/now_v8/lib/src/services/cmd/colors_service/colors_service.dart
@@ -5,17 +5,49 @@ import 'package:now_v8/src/core/models/spotColors.dart';
 
 class ColorsService implements IColorService{
   int index = 0;
-  List<SpotsColors> _availableColors =[];
-  late SpotsColors grey;
-
-  ColorsService() {
-    populateColors();
-  
-    grey = SpotsColors(
+  final List<SpotsColors> _availableColors = [ 
+      SpotsColors(
+        color: Colors.red,
+        hue: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+      ),
+      SpotsColors(
+        color: Colors.blue,
+        hue: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure),
+      ),
+      SpotsColors(
+        color: Colors.cyan,
+        hue: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueCyan),
+      ),
+      SpotsColors(
+        color: Colors.green,
+        hue: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+      ),
+      SpotsColors(
+        color: Colors.pink,
+        hue: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueMagenta),
+      ),
+      SpotsColors(
+        color: Colors.purple,
+        hue: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet),
+      ),
+      SpotsColors(
+        color: Colors.orange.shade800,
+        hue: BitmapDescriptor.defaultMarkerWithHue(1),
+      ),
+      SpotsColors(
+        color: Colors.blue.shade700,
+        hue: BitmapDescriptor.defaultMarkerWithHue(200),
+      ),
+      SpotsColors(
+        color: Colors.pink.shade700,
+        hue: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRose),
+      ),];
+  final SpotsColors grey = SpotsColors(
         color: Colors.grey.shade400,
         hue: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueOrange),
-      );
-  }
+  );
+
+  ColorsService();
 
   @override
   SpotsColors getColor() {
@@ -34,72 +66,5 @@ class ColorsService implements IColorService{
   @override
   SpotsColors getScheduleColor() {
     return _availableColors[_availableColors.length -1 ];
-  }
-
-  void populateColors() {
-
-    _availableColors.add(
-      SpotsColors(
-        color: Colors.red,
-        hue: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
-      ),
-    );
-
-    _availableColors.add(
-      SpotsColors(
-        color: Colors.blue,
-        hue: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure),
-      ),
-    );
-
-    _availableColors.add(
-      SpotsColors(
-        color: Colors.cyan,
-        hue: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueCyan),
-      ),
-    );
-
-    _availableColors.add(
-      SpotsColors(
-        color: Colors.green,
-        hue: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
-      ),
-    );
-
-    _availableColors.add(
-      SpotsColors(
-        color: Colors.pink,
-        hue: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueMagenta),
-      ),
-    );
-
-    _availableColors.add(
-      SpotsColors(
-        color: Colors.purple,
-        hue: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet),
-      ),
-    );
-
-    _availableColors.add(
-      SpotsColors(
-        color: Colors.orange.shade800,
-        hue: BitmapDescriptor.defaultMarkerWithHue(1),
-      ),
-    );
-
-    _availableColors.add(
-      SpotsColors(
-        color: Colors.blue.shade700,
-        hue: BitmapDescriptor.defaultMarkerWithHue(200),
-      ),
-    );
-
-    _availableColors.add(
-      SpotsColors(
-        color: Colors.pink.shade700,
-        hue: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRose),
-      ),
-    );
-
   }
 }
