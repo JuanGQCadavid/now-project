79335ea90beace1f0f98dea33249663541256620
Juan Gonzalo Quıroz Cadavıd <jquirozcadavid@gmail.com>
2024-08-13 17:19:09 +0300

    :construction: Another unit test damn for emitRecords. Refactor in the old one

--- services/scheduledPatternsChecker/cmd/lambda/main_test.go
+++ services/scheduledPatternsChecker/cmd/lambda/main_test.go
@@ -1,85 +1,149 @@
 package main
 
 import (
-	"os"
 	"testing"
 
-	"github.com/JuanGQCadavid/now-project/services/pkgs/common/logs"
-	credFinderCore "github.com/JuanGQCadavid/now-project/services/pkgs/credentialsFinder/core/core/domain"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
-	"github.com/stretchr/testify/mock"
 )
 
-func init() {
-	os.Setenv("neo4jUser", "DUMMY")
-	os.Setenv("neo4jPassword", "DUMMY")
-	os.Setenv("neo4jUri", "DUMMY")
-}
+var (
+	scheduleBody string = `
+	{
+		"scheduleId":"Holi"
+	}
+	`
 
-// Mock implementation of the SSMCredentialsFinder interface
-type MockSSMCredentialsFinder struct {
-	mock.Mock
-}
+	generateBatchBody string = `
+	{
+		"Operation":"generateDatesFromSchedulePatterns",
+		"TimeWindow": 60
+	}
+	`
+)
 
-func (m *MockSSMCredentialsFinder) FindNeo4jCredentialsFromDefaultEnv() (neo4j.Driver, error) {
-	args := m.Called()
-	return args.Get(0).(neo4j.Driver), args.Error(1)
-}
-func (m *MockSSMCredentialsFinder) GetDBCredentialsFromDefaultEnv() (*credFinderCore.DBCredentials, error) {
-	args := m.Called()
-	return args.Get(0).(*credFinderCore.DBCredentials), args.Error(1)
-}
-
-// mockCredsFinder := new(MockSSMCredentialsFinder)
-// // mockDriver := nil // Replace with your actual driver type
-// mockCredsFinder.On("FindNeo4jCredentialsFromDefaultEnv").Return(nil, nil)
-
-func TestEmitRecords(t *testing.T) {
+func TestEmitRecordsEventsDispatched(t *testing.T) {
 	var (
-		done              chan interface{} = make(chan interface{})
-		generateBatchBody string           = `
-		{
-			"Operation":"generateDatesFromSchedulePatterns",
-			"TimeWindow": 60
-		}
-		`
-		scheduleBody string = `
-		{
-			"scheduleId":"Holi"
-		}
-		`
-		payload events.SQSEvent = events.SQSEvent{
-			Records: []events.SQSMessage{
-				{ // one with Operation set in message attributes
-					MessageAttributes: map[string]events.SQSMessageAttribute{
-						Operation: {
-							StringValue: aws.String(string(SchedulePatternAppended)),
-						},
-					},
-					Body: scheduleBody,
-				},
-				{ // one with Operation set in body
-					Body: generateBatchBody,
-				},
-				{ // one with Operation set in message attributes but wrong, leaving it on body
-					MessageAttributes: map[string]events.SQSMessageAttribute{
-						Operation: {
-							StringValue: aws.String("AmIWrong"),
-						},
-					},
-					Body: generateBatchBody,
-				},
-			},
-		}
+		done chan interface{} = make(chan interface{})
 	)
-	resultingChan := emitRecords(done, &payload)
 
-	for value := range resultingChan {
-		logs.Info.Printf("Op: %s, body: %s \n", string(value.operation), value.body)
+	var singleRecord = events.SQSMessage{
+		MessageAttributes: map[string]events.SQSMessageAttribute{
+			Operation: {
+				StringValue: aws.String(string(SchedulePatternAppended)),
+			},
+		},
+		Body: scheduleBody,
 	}
 
-	logs.Info.Println("Done")
+	var tests = []struct {
+		name          string
+		baseRecord    events.SQSMessage
+		numberOfTimes int
+	}{
+		{
+			"0 records where sent",
+			singleRecord,
+			0,
+		},
+		{
+			"1 record where sent",
+			singleRecord,
+			1,
+		},
+		{
+			"1000 record where sent",
+			singleRecord,
+			1000,
+		},
+	}
 
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			payload := make([]events.SQSMessage, tt.numberOfTimes)
+
+			for i := 0; i < tt.numberOfTimes; i++ {
+				payload[i] = tt.baseRecord
+			}
+
+			resultingChan := emitRecords(done, &events.SQSEvent{Records: payload})
+
+			var counter int
+			for range resultingChan {
+				counter++
+			}
+			if counter != tt.numberOfTimes {
+				t.Errorf("got %d, want %d", counter, tt.numberOfTimes)
+			}
+
+		})
+	}
+}
+
+func TestEmitRecordsOperation(t *testing.T) {
+	var (
+		done chan interface{} = make(chan interface{})
+	)
+
+	var tests = []struct {
+		name              string
+		payload           events.SQSEvent
+		operationExpected Operations
+	}{
+		{
+			"Operation taken from the message attributes",
+			events.SQSEvent{
+				Records: []events.SQSMessage{
+					{
+						MessageAttributes: map[string]events.SQSMessageAttribute{
+							Operation: {
+								StringValue: aws.String(string(SchedulePatternAppended)),
+							},
+						},
+						Body: scheduleBody,
+					},
+				},
+			},
+			SchedulePatternAppended,
+		},
+		{
+			"Operation taken from the message body",
+			events.SQSEvent{
+				Records: []events.SQSMessage{
+					{
+						Body: generateBatchBody,
+					},
+				},
+			},
+			GenrateDatesFromSchedulePatterns,
+		},
+		{
+			"Operation taken from the message body omitting attributes",
+			events.SQSEvent{
+				Records: []events.SQSMessage{
+					{ // one with Operation set in message attributes but wrong, leaving it on body
+						MessageAttributes: map[string]events.SQSMessageAttribute{
+							Operation: {
+								StringValue: aws.String("AmIWrong"),
+							},
+						},
+						Body: generateBatchBody,
+					},
+				},
+			},
+			GenrateDatesFromSchedulePatterns,
+		},
+	}
+	// The execution loop
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			resultingChan := emitRecords(done, &tt.payload)
+			ans := <-resultingChan
+			if ans.operation != tt.operationExpected {
+				t.Errorf("got %s, want %s", string(ans.operation), string(tt.operationExpected))
+			}
+		})
+	}
 }
