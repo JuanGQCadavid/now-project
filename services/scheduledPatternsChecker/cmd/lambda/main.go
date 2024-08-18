730b210a4acf77673667a74e01a5c64cc50b99c9
Juan Gonzalo Quıroz Cadavıd <jquirozcadavid@gmail.com>
2024-08-18 17:23:35 +0300

    :construction: WIP schedule events identifer and workers added, missing testing

--- services/scheduledPatternsChecker/cmd/lambda/main.go
+++ services/scheduledPatternsChecker/cmd/lambda/main.go
@@ -223,6 +223,15 @@ type Record struct {
 	operation Operations
 }
 
+type Result struct {
+	err error
+}
+
+// type PipelineEventConfig struct {
+// 	operations []Operations
+// 	pipeline   func(<-chan interface{}, <-chan Record) chan Result
+// }
+
 func emitRecords(done <-chan interface{}, source *events.SQSEvent) <-chan Record {
 	valueStream := make(chan Record)
 
@@ -257,6 +266,125 @@ func emitRecords(done <-chan interface{}, source *events.SQSEvent) <-chan Record
 	return valueStream
 }
 
+func creationOfDates(done <-chan interface{}, events <-chan domain.Spot) <-chan Result {
+	out := make(chan Result)
+	go func() {
+
+		// toCreate = append(toCreate, utils.FromSpotRequestToSpot(body.SpotRequest))
+
+		toCreate := make([]domain.Spot, 0, len(events))
+		if len(toCreate) > 0 {
+
+			logs.Info.Println("Dates to create from schedules Id:")
+			for _, spot := range toCreate {
+				logs.Info.Printf("Scheudle Id %+v \n", spot)
+			}
+
+			if _, errs := srv.CreateScheduledDatesFromSchedulePattern(toCreate, DefaultTimeWindow); errs != nil {
+				for err, errSpot := range errs {
+					logs.Error.Println("To create Error ", err.Error(), " on ", errSpot)
+				}
+			}
+
+		}
+	}()
+
+	return out
+}
+
+func deletionOfDates(done <-chan interface{}, events <-chan string) <-chan Result {
+	out := make(chan Result)
+
+	go func() {
+		defer close(out)
+
+		toDelete := make([]string, 0, len(events))
+
+		for event := range events {
+			toDelete = append(toDelete, event)
+		}
+
+		if len(toDelete) > 0 {
+			logs.Info.Println("Dates to delete from schedules Id:")
+			for _, spot := range toDelete {
+				logs.Info.Printf("Scheudle Id %s \n", spot)
+			}
+			err := srv.DeleteScheduleDatesFromSchedulePattern(toDelete)
+
+			if err != nil {
+				logs.Error.Println("We found the next error ", err.Error())
+			}
+
+			select {
+			case <-done:
+				return
+			case out <- Result{
+				err: err,
+			}:
+
+			}
+		}
+	}()
+	return out
+}
+
+func scheduleEventsIdentifer(done <-chan interface{}, events <-chan Record) (<-chan domain.Spot, <-chan string) {
+	createStream, deleteStream := make(chan domain.Spot), make(chan string)
+
+	go func() {
+		defer close(createStream)
+		defer close(deleteStream)
+
+		for record := range events {
+
+			var body utils.Body
+			err := json.Unmarshal([]byte(record.body), &body)
+
+			if err != nil {
+				logs.Error.Println("Oh shit, aborting this record due to:", err.Error())
+				continue
+			}
+
+			if record.operation == SchedulePatternAppended || record.operation == SchedulePatternResumed {
+				if (len(body.SpotRequest.SpotInfo.SpotId) + len(body.SpotRequest.SpotPatterns)) == 0 {
+					logs.Error.Println("SpotRequest is empty")
+					continue
+				}
+
+				select {
+				case <-done:
+					return
+				case createStream <- utils.FromSpotRequestToSpot(body.SpotRequest):
+				}
+
+			} else if record.operation == SchedulePatternConcluded || record.operation == SchedulePatternFreezed {
+				if len(body.ScheduleId) == 0 {
+					logs.Error.Println("ScheduleId is empty")
+					continue
+				}
+
+				select {
+				case <-done:
+					return
+				case deleteStream <- body.ScheduleId:
+				}
+
+			} else {
+				logs.Warning.Println("Operation not recognized, aborting message")
+			}
+
+		}
+
+	}()
+
+	return createStream, deleteStream
+}
+
+// func filterRecords(done <-chan interface{}, events <-chan Record, configMap map[string]PipelineEventConfig) map[string]chan Result {
+
+// 	return nil
+// }
+
 func HandlerV2(ctx context.Context, body *events.SQSEvent) (string, error) {
 
 	// New code
@@ -266,17 +394,8 @@ func HandlerV2(ctx context.Context, body *events.SQSEvent) (string, error) {
 
 	emitRecords(done, body)
 
-	// Old code copied
-	toDelete := make([]string, 0, MaxBatchSize)
-	toCreate := make([]domain.Spot, 0, MaxBatchSize)
-
+	// filter(done, chan of results, configmap) - a map of chans, one per config in the configmap
 	for _, record := range body.Records {
-
-		log.Printf("%+v \n", record)
-		log.Println("------------")
-		log.Printf("%+v \n", record.Body)
-
-		log.Println(" ********** ")
 
 		operation := GetOperationNameFromAttributes(record)
 
@@ -323,66 +442,6 @@ func HandlerV2(ctx context.Context, body *events.SQSEvent) (string, error) {
 			// TODO: Here, what to do when pending dates to close is invoked
 			continue
 		}
-
-		var body utils.Body
-		err := json.Unmarshal([]byte(record.Body), &body)
-
-		if err != nil {
-			logs.Error.Println("Oh shit, aborting this record due to:", err.Error())
-			continue
-		}
-
-		// SCHEDULE PATTERNS SNS
-
-		if operation == SchedulePatternAppended || operation == SchedulePatternResumed {
-
-			if (len(body.SpotRequest.SpotInfo.SpotId) + len(body.SpotRequest.SpotPatterns)) == 0 {
-				logs.Error.Println("SpotRequest is empty")
-				continue
-			}
-
-			toCreate = append(toCreate, utils.FromSpotRequestToSpot(body.SpotRequest))
-
-		} else if operation == SchedulePatternConcluded || operation == SchedulePatternFreezed {
-
-			if len(body.ScheduleId) == 0 {
-				logs.Error.Println("ScheduleId is empty")
-				continue
-			}
-
-			toDelete = append(toDelete, body.ScheduleId)
-		} else {
-			logs.Warning.Println("Operation not recognized, aborting message")
-
-		}
-	}
-
-	if len(toDelete) > 0 {
-		logs.Info.Println("Dates to delete from schedules Id:")
-		for _, spot := range toDelete {
-			logs.Info.Printf("Scheudle Id %s \n", spot)
-		}
-
-		err := srv.DeleteScheduleDatesFromSchedulePattern(toDelete)
-
-		if err != nil {
-			logs.Error.Println("We found the next error ", err.Error())
-		}
-
-	}
-
-	if len(toCreate) > 0 {
-		logs.Info.Println("Dates to create from schedules Id:")
-		for _, spot := range toCreate {
-			logs.Info.Printf("Scheudle Id %+v \n", spot)
-		}
-
-		if _, errs := srv.CreateScheduledDatesFromSchedulePattern(toCreate, DefaultTimeWindow); errs != nil {
-			for err, errSpot := range errs {
-				logs.Error.Println("To create Error ", err.Error(), " on ", errSpot)
-			}
-		}
-
 	}
 
 	return AcknowledgeKeyworkd, nil
