775ba096758adeae9fdf35579465364f7de7373d
Juan Gonzalo Quıroz Cadavıd <jquirozcadavid@gmail.com>
2025-02-06 15:07:53 +0200

    :art: Starting with the theme data thing

--- front/now_v8/lib/main.dart
+++ front/now_v8/lib/main.dart
@@ -19,8 +19,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
+        // theme: ThemeData(
+        //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        //   useMaterial3: true,
+        // ),
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          scaffoldBackgroundColor: Colors.white,
           useMaterial3: true,
         ),
         home:
